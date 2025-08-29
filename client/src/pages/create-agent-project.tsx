8aebebe56bc15b04b201fd243207681722238f50
paribaker <58012003+paribaker@users.noreply.github.com>
2025-08-29 14:39:52 -0700

    Agent Instance Filters (#34)
    ## What this does
    
    Add a description of what feature/page this PR intends to add or change.
    
    - [ticket title](URL to story/issue/ticket goes here)
    
    ## Checklist
    - [ ] Add ticket number to PR title (if applicable)
    - [ ] Todo 2
    - [ ] Todo 3
    
    
    ## Deploy Notes
    
    Add any notes for changes needed when deployed. I.e. API keys,
    environment variables, database changes, etc.
    
    ## How to test
    
    Add user steps to achieve desired functionality for this feature.
    
    ## Django Admin
    | user | password | has admin | notes |
    | --- | --- | --- | --- |
    | `admin@thinknimble.com` | <ENTERAPASSWORD> | :white_check_mark: | |
    | `playwright@thinknimble.com` | <ENTERAPASSWORD> | :x: | Only use for
    automated E2E testing |
    
    ---------
    
    Co-authored-by: Pari Work Temp <pariwork@thinknimble.com>

--- client/src/pages/create-agent-project.tsx
+++ client/src/pages/create-agent-project.tsx
@@ -378,7 +378,7 @@ const AgentInstanceInner = ({
       const formValue = form.value
       const data = {
         ...formValue,
-        agentProject: agentProject.id,
+        projects: [agentProject.id],
         provider: formValue.provider?.value,
         agentType: formValue.agentType?.value,
       }
@@ -542,7 +542,7 @@ export const CreateAgentProject = () => {
   )
 
   const { data: projectInstances, isLoading: loadingInstances } = useQuery(
-    agentInstanceQueries.list(new Pagination(), { agentProject: id }),
+    agentInstanceQueries.list(new Pagination(), { projects: [id ?? ''] }),
   )
 
   useEffect(() => {
